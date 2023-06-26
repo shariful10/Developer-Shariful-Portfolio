1cf7880643fac152545b3027e4fe42170f27d50c
Sk Shariful Islam <skshariful10@gmail.com>
2023-06-26 14:32:11 +0600

    Modify Social Navbar

--- src/Shared/SocialIcons/SocialIcons.jsx
+++ src/Shared/SocialIcons/SocialIcons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaFacebook } from "react-icons/fa";
 import { BsFillPersonLinesFill, BsLinkedin } from "react-icons/bs";
 import { TbBrandDribbbleFilled } from "react-icons/tb";
 import Container from "../Container/Container";
@@ -9,32 +9,44 @@ const SocialIcons = () => {
 		<Container>
 			<div className="flex fixed flex-col top-[35%] left-0">
 				<ul>
-					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-rose-600">
+					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-red-600">
 						<a
 							className="flex justify-between items-center w-full text-white px-5"
-							href="/">
+							href="https://drive.google.com/file/d/1xAeBuYsELJaZ9uG7RdptiM-3v0n1spB4/view?usp=sharing"
+							target="_blank">
 							Resume <BsFillPersonLinesFill size={24} />
 						</a>
 					</li>
 					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-black">
 						<a
 							className="flex justify-between items-center w-full text-white px-5"
-							href="/">
+							href="https://github.com/shariful10"
+							target="_blank">
 							GitHub <FaGithub size={24} />
 						</a>
 					</li>
 					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-blue-600">
 						<a
 							className="flex justify-between items-center w-full text-white px-5"
-							href="/">
+							href="https://www.linkedin.com/in/md-shariful-islam-3132721b8/"
+							target="_blank">
 							LinkedIn <BsLinkedin size={24} />
 						</a>
 					</li>
-					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-[#b269f6]">
+					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-[#ea4c89]">
 						<a
 							className="flex justify-between items-center w-full text-white px-5"
-							href="/">
+							href="https://dribbble.com/shariful102"
+							target="_blank">
 							Dribbble <TbBrandDribbbleFilled size={24} />
+						</a>
+					</li>
+					<li className="text-white w-[170px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-1px] duration-300 bg-blue-600">
+						<a
+							className="flex justify-between items-center w-full text-white px-5"
+							href="https://www.facebook.com/skshariful.islam.18"
+							target="_blank">
+							Facebook <FaFacebook size={24} />
 						</a>
 					</li>
 				</ul>
