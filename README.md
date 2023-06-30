abcd054e78b2cf079388099c420e6447e51b346b
Sk Shariful Islam <skshariful10@gmail.com>
2023-07-01 01:16:58 +0600

    README.md File Added

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ### Live Preview: [Developer Shariful](https://developer-shariful.netlify.app/)
 
+### Features:
+* This is a personal portfolio website.
+* There are total five section which is Home, Skills, Projects, Testimonials and Contact Section.
+* There are also navbar & footer.
+* You can know me from this website.
+
 ### Used Packages:
 * aos
 * swiper
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
 				Montserrat: ["Montserrat", "sans-serif"],
 			},
 			backgroundImage: {
-				bodyBg: "url('./src/assets/bg-texture.png')",
+				bodyBg: "url(https://i.ibb.co/zxkWd11/bg-texture.png)",
 				project1: "url(https://i.ibb.co/Y3kkVhV/justice-legalsolutions.png)",
 				project2: "url(https://i.ibb.co/9nmB06t/bistro.png)",
 				project3: "url(https://i.ibb.co/5jwgYdD/sports-gear.png)",
